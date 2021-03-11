17d65b65ea693d3dd327bda46e2f69a54e02ef2b
Hamzabyte96 <brcaninovichamza@yahoo.com>
2021-03-11 22:15:35 +0100

    izmjene u ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    var myFirstName = "Hamza";
+    var myLastName = "Brcaninovic";
+    var myHomeTown = "Zivinice";
+return {
+    myFirstName,
+    myLastName,
+    myHomeTown
+
+}
+
+}
+console.log(main());
+module.exports = main;
